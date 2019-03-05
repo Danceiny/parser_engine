f8cf3aa517f4c5b60169a23210b0816943ac9456
Danceiny <danceiny@gmail.com>
2019-03-05 19:55:34 +0800

    find config

--- parser_engine/utils.py
+++ parser_engine/utils.py
@@ -49,11 +49,10 @@ def load_config_data():
         if not os.path.isabs(config_path):
             config_path1 = closest_parser_engine_json(config_path)
             if not config_path1:
-                print("try to find default config", __file__)
-                config_path1 = closest_parser_engine_json(config_path, os.path.dirname(__file__))
+                default_config_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates")
+                config_path1 = closest_parser_engine_json(config_path, default_config_dir)
                 if not config_path1:
                     raise FileNotFoundError
-            else:
-                config_path = config_path1
-        with open(config_path) as f:
+            config_path = config_path1
+        with open(config_path, mode='rb') as f:
             return json.loads(f.read())
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8
 from parser_engine.utils import *
 
 if __name__ == '__main__':
     p = closest_parser_engine_json('.json')
     assert p == ''
+
+    data = load_config_data()
